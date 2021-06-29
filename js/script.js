a4d18a23b9a3fbef6324c806df1595d796804d2e
diego <doan@gmail.com>
2021-06-29 17:01:05 +0700

    make side bar for tablet and mobile screen

---
+++ js/script.js
@@ -0,0 +1,23 @@
+//header-sidebar
+function showHeaderSidebar() {
+    document.getElementById("header__sidebar-area").style.display = "block";
+    document.getElementById("header_sidebar-opacity").style.left = "0";
+    document.getElementById("header__sidebar").style.right = "0";
+    
+
+}
+
+function hideHeaderSidebar() {
+    document.getElementById("header__sidebar-area").style.display = "none";
+}
+
+//footer-sidebar
+function showFooterSidebar() {
+    document.getElementById("footer__sidebar-area").style.display = "block";
+    document.getElementById("footer__sidebar").style.right = "0";
+    
+}
+
+function hideFooterSidebar() {
+    document.getElementById("footer__sidebar-area").style.display = "none";
+}
